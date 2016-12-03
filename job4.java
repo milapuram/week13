b940362f36b440c32cc01c76cd8c525870087b88
milapuram <manuuu.9494@gmail.com>
2016-12-03 11:33:24 -0600

    Update job4.java

--- job4.java
+++ job4.java
@@ -14,9 +14,9 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class job4 {
 
   public static class Mapper1
-       extends Mapper<LongWritable, Text, Text, IntWritable>{
+       extends Mapper<LongWritable, Text, Text, NullWritable>{
 
-    private final static IntWritable one = new IntWritable(1);
+     NullWritable one = NullWritable.get();
     private Text cited = new Text();
 
     public void map(LongWritable key, Text value, Context context) throws IOException,InterruptedException {
@@ -27,13 +27,12 @@ public class job4 {
         String[] line = value.toString().split("\\s");
         if (line[0].charAt(0)!='#' && line[10].equals("404")){
             cited.set(line[4]);
-         context.write(cited, one);
+         context.write(cited,NullWritable.get());
         }
     }
   }
- 
-  public static class Reducer1
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
+/* public static class Reducer1
+       extends Reducer<Text,NullWritable,Text,NullWritable> {
     private IntWritable result = new IntWritable();
 
     public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
@@ -44,21 +43,23 @@ public class job4 {
       result.set(sum);
       context.write(key, result);
     }
-  }
+  }*/
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     Job job = Job.getInstance(conf, "job1");
     job.setJarByClass(job4.class);
     job.setMapperClass(Mapper1.class);
-    job.setCombinerClass(Reducer1.class);
-    job.setReducerClass(Reducer1.class);
+  //  job.setCombinerClass(Reducer1.class);
+    //job.setReducerClass(Reducer1.class);
+    job.setNumReduceTasks(0);
     //job.setSortComparatorClass(SortKeyComparator.class);
     job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
+    job.setOutputValueClass(NullWritable.class);
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
+
 
