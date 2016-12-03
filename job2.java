ac6e1c1ba1e0580625d28278ef2c17139b275520
milapuram <manuuu.9494@gmail.com>
2016-12-03 11:09:28 -0600

    Update job2.java

--- job2.java
+++ job2.java
@@ -1,122 +1,73 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+//FrequentPage
 import java.io.IOException;
-import java.net.URI;
-import java.util.*;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.*;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.Counter;
-import org.apache.hadoop.util.GenericOptionsParser;
-import org.apache.hadoop.util.StringUtils;
 
- public class job2{
-     public static class Mapper1
-       extends Mapper<LongWritable, Text, Text, IntWritable>{
+public class job2 {
 
-    //static enum CountersEnum { INPUT_WORDS }
+  public static class Mapper1 extends Mapper<Object, Text, Text, IntWritable>{
 
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
 
-    private boolean caseSensitive;
-    private Set<String> patternsToSkip = new HashSet<String>();
-
-    private Configuration conf;
-    private BufferedReader fis;
-
-    @Override
-    public void setup(Context context) throws IOException,
-        InterruptedException {
-      conf = context.getConfiguration();
-      caseSensitive = conf.getBoolean("wordcount.case.sensitive", true);
-      if (conf.getBoolean("wordcount.skip.patterns", true)) {
-        URI[] patternsURIs = Job.getInstance(conf).getCacheFiles();
-        for (URI patternsURI : patternsURIs) {
-          Path patternsPath = new Path(patternsURI.getPath());
-          String patternsFileName = patternsPath.getName().toString();
-          parseSkipFile(patternsFileName);
-        }
+    public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+      String[] line = value.toString().split("\\s");;
+          if (line[0].charAt(0) != '#'&& !line[4].equals("/"))
+          {
+             word.set(line[4]);
+             context.write(word, one);
+                }
       }
-    }
 
-    private void parseSkipFile(String fileName) {
-      try {
-        fis = new BufferedReader(new FileReader(fileName));
-        String pattern = null;
-        while ((pattern = fis.readLine()) != null) {
-          patternsToSkip.add(pattern);
-        }
-      } catch (IOException ioe) {
-      System.err.println("Caught exception while parsing the cached file '"
-            + StringUtils.stringifyException(ioe));
-      }
-    }
+        
+  } 
+  public static class Reducer1 extends Reducer<Text, IntWritable, Text, IntWritable> {
 
-    @Override
-    public void map(LongWritable key, Text value, Context context
-                    ) throws IOException, InterruptedException {
-      String line = (caseSensitive) ?
-          value.toString() : value.toString().toLowerCase();
-        for (String pattern : patternsToSkip) {
-        line = line.replaceAll(pattern, "");
-      }
-     String[] st = value.toString().split("\\s");
-            if (st.length>10){
-            word.set(st[4]);
-            context.write(word, one);
-      }
-    }
-}
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
+        private static int fvalue = 0;
+        private static Text fkey = new Text();
+        private static IntWritable max_value = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
       int sum = 0;
       for (IntWritable val : values) {
         sum += val.get();
       }
-      result.set(sum);
-      context.write(key, result);
+          if(sum > fvalue)
+          {
+                  fvalue = sum;
+                  fkey.set(key);
+                  max_value.set(sum);
+          }
     }
-  }
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException
+        {
+                context.write(fkey, fvalue);
+        }
+  } // end of Reducer class
+
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
-    GenericOptionsParser optionParser = new GenericOptionsParser(conf, args);
-    String[] remainingArgs = optionParser.getRemainingArgs();
-    if (!(remainingArgs.length != 2 || remainingArgs.length != 4)) {
-      System.err.println("Usage: wordcount <in> <out> [-skip skipPatternFile]");
-      System.exit(2);
-    }
-    Job job = Job.getInstance(conf, "word count");
-    job.setJarByClass(job2:21.class);
+    Job job = Job.getInstance(conf, "Job 2 FrequentPage");
+    job.setJarByClass(job2.class);
+
     job.setMapperClass(Mapper1.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
-    job.setOutputKeyClass(Text.class);
+    job.setCombinerClass(Reducer1.class);
+    job.setReducerClass(Reducer1.class);
+
+        job.setOutputKeyClass(Text.class);
     job.setOutputValueClass(IntWritable.class);
 
-    List<String> otherArgs = new ArrayList<String>();
-    for (int i=0; i < remainingArgs.length; ++i) {
-      if ("-skip".equals(remainingArgs[i])) {
-        job.addCacheFile(new Path(remainingArgs[++i]).toUri());
-        job.getConfiguration().setBoolean("wordcount.skip.patterns", true);
-      } else {
-        otherArgs.add(remainingArgs[i]);
-      }
-  }
-    FileInputFormat.addInputPath(job, new Path(otherArgs.get(0)));
-    FileOutputFormat.setOutputPath(job, new Path(otherArgs.get(1)));
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
